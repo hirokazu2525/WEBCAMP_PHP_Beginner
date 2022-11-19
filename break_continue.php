f34f5109da373fef31a651fb256bbf31452275ae
kazumahirosawa <kazumahirosawa@example.com>
2022-11-19 05:05:25 +0000

    break_continue.php

---
+++ break_continue.php
@@ -0,0 +1,26 @@
+<?php
+//continueの確認
+echo "\n continueの確認 \n";
+for($i = 0; $i < 10; ++$i) {
+    echo "{$i} は奇数かな？ \n";
+    if ( ($i % 2) === 1 ) {
+        //奇数なのでcontinueする(以降の処理をスキップする)
+        continue;
+    }
+    //continueを通らなければこの処理が動く
+    echo "偶数でした！ \n";
+}
+//breakの確認
+echo "breakの確認 \n";
+$i = 0;
+while($i < 10) {
+    echo "{$i} \n";
+    //
+    if ($i >= 5) {
+        //ループを抜ける
+        echo "{$i}なのでループをを抜けます \n";
+        break;
+    }
+    //
+    ++$i;
+}
