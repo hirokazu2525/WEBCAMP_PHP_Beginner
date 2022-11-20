5d08b52e49268e9df87af06b033b0ad317602be6
kazumahirosawa <kazumahirosawa@example.com>
2022-11-20 16:40:42 +0000

    passed_by_value.phpを書いた

---
+++ passed_by_value.php
@@ -0,0 +1,14 @@
+<?php
+//関数の宣言
+function myFunc(int $val)
+{
+    //$iの値を2倍にする
+    $val *= 2; // "$i = $i * 2"と同じ意味を持つ、複合演算子という書き方です
+    //値を確認する
+    echo "in myFunc, val is {$val} \n";
+}
+//変数への代入
+$i = 10;
+myFunc($i);
+//変数iの中身の確認
+echo "i is {$i} \n";
